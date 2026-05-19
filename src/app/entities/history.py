32292e288411d43429b18b784d1bf47a1a2c1818
Diego Piol Amancio <25.00475-5@maua.br>
2026-05-19 10:35:34 -0300

    feat: add static methods

--- src/app/entities/history.py
+++ src/app/entities/history.py
@@ -5,3 +5,37 @@ class History:
     timestamp: float
 
 
+    @staticmethod
+    def validate_type(type: str) -> Tuple[bool, str]:
+        if type is None:
+            return (False, "Type is required")
+        if type(type) != str:
+            return (False, "Type must be a string")
+        return (True, "")
+    
+    @staticmethod
+    def validate_value(value: float) -> Tuple[bool, str]:
+        if value is None:
+            return (False, "Value is required")
+        if type(value) != float:
+            return (False, "Value must be a float")
+        if value < 0:
+            return (False, "Value must be a positive number")
+        return (True, "")
+    
+    @staticmethod
+    def current_balance(current_balance: float) -> Tuple[bool, str]:
+        if current_balance is None:
+            return (False, "Current balance is required")
+        if type(current_balance) != float:
+            return (False, "Current balance must be a float")
+        return (True, "")
+    
+    @staticmethod
+    def validate_timestamp(timestamp: float) -> Tuple[bool, str]:
+        if timestamp is None:
+            return (False, "Timestamp is required")
+        if type(timestamp) != float:
+            return (False, "Timestamp must be a float")
+        return (True, "")
+    
