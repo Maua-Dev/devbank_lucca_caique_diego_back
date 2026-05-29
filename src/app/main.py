3d87a70eea5256b29f560905ae2cb86f73191ab3
Diego Piol <digopiol@gmail.com>
2026-05-29 16:06:10 -0300

    feat: adiciona funcao .get/

--- src/app/main.py
+++ src/app/main.py
@@ -13,10 +13,15 @@ from .entities.item import Item
 app = FastAPI()
 
 repo = Environments.get_item_repo()()
+user_repo = Environments.get_user_repo()()
 
 # a baixo estão as rotas da api
 # elas interagem com os métodos de repositório. por exemplo a rota create item chama, não exclusivamente,
 # o método repo.create_item() para criar o item no nosso repositório
+@app.get("/")
+def execute_get_pra_barra():
+    return user_repo.get_first_user()
+
 
 @app.get("/items/get_all_items")
 def get_all_items():
